6f088fbc7aa2dc273cff09aecf4f4dab35dafa5e
2321w <149325251+2321w@users.noreply.github.com>
2025-07-10 12:46:53 -0500

    Add files via upload

---
+++ Ejercicios_google.py
@@ -0,0 +1,12 @@
+#Calcular el area de un cuadrado 
+Longitud=input("Longitud: ")
+
+Area=float(Longitud)*float(Longitud)
+
+Perimetro=float(Longitud)+float(Longitud)+float(Longitud)+float(Longitud)
+
+
+print(f"El area es {Area}")
+
+
+print(f"El perimetro es {Perimetro}")
